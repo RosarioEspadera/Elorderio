6f385c72c824630a9eaf0faaa3bd999d850a529d
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-30 05:39:51 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 const supabase = createClient('https://bcmibfnrydyzomootwcb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbWliZm5yeWR5em9tb290d2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4MDg3MzQsImV4cCI6MjA2OTM4NDczNH0.bu4jf3dH07tvgUcL0laZJnmLGL6nPDo4Q9XRCXTBO9I');
 
 // 🔐 Login function

5eea1601962bd1712e7689ae8a914240ef534b27
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-01 00:15:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
 const dishMenu = document.getElementById("dish-menu");
 const form = document.getElementById("order-form");
 const cartTable = document.getElementById("cart-table").querySelector("tbody");
@@ -115,6 +117,7 @@ form.addEventListener("submit", function(e) {
     });
 });
 // 🔑 Supabase Auth client
+
 const supabase = createClient('https://bcmibfnrydyzomootwcb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbWliZm5yeWR5em9tb290d2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4MDg3MzQsImV4cCI6MjA2OTM4NDczNH0.bu4jf3dH07tvgUcL0laZJnmLGL6nPDo4Q9XRCXTBO9I');
 
 // ✍️ Sign Up
